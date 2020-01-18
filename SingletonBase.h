2d8cf175b57b44aee43809e26f870d5477c9ec19
920216044@qq.com <920216044@qq.com>
2020-01-18 20:28:07 +0800

    单例基类 所有继承此类即成为单例

---
+++ SingletonBase.h
@@ -0,0 +1,29 @@
+#ifndef _SINGLETON_BASE_H_
+#define _SINGLETON_BASE_H_
+
+// 单例基类
+template <typename sgt_class>
+class SingletonBase
+{
+public:
+	inline static sgt_class* GetInstance(void)
+	{
+		static sgt_class ins;
+		return &ins;
+	}
+
+protected:
+	SingletonBase(void) {}
+
+	virtual ~SingletonBase(void) {}
+
+	SingletonBase(const SingletonBase& that) = delete;
+
+	SingletonBase(SingletonBase&& that) = delete;
+
+	SingletonBase& operator = (const SingletonBase& that) = delete;
+
+	SingletonBase& operator = (SingletonBase&& that) = delete;
+};
+
+#endif // !_SINGLETON_BASE_H_
