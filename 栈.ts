d4e517fa18ccc3c2b79b353e9a25bb5b0599c238
920216044@qq.com <920216044@qq.com>
2020-01-21 21:27:15 +0800

    栈 双向链表实现

---
+++ 栈.ts
@@ -0,0 +1,22 @@
+import { LinkList } from "./双向缓冲链表";
+
+// 栈
+// 先进后出
+// 封装双向链表
+export class Stack extends LinkList<any>{
+    constructor(){
+        super();
+    }
+    
+    push(data){
+        this.insert(data, 0);
+    }
+    
+    pop(){
+        super.pop(false);
+    }
+
+    get(){
+        return super.get(0);
+    }
+}
