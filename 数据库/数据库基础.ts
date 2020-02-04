016e49bcf15bf740f46d2ad8cbf78ff8289f700e
920216044@qq.com <920216044@qq.com>
2020-02-04 18:45:23 +0800

    数据库

---
+++ 数据库/数据库基础.ts
@@ -0,0 +1,13 @@
+/**
+ * 参考网站: https://www.cnblogs.com/mangM/p/9628674.html#1351042391
+ * 数据库本质是数据存储
+ * 数据 data
+ * 数据库 DB: data base
+ * 数据库管理系统 DBMS: data base management system
+ * 数据库系统 DBS: data base system
+ * 数据库管理系统 数据库 数据表 数据的关系
+ * 管理系统： 关系数据库的软件
+ * 数据库：文件夹
+ * 数据表：文件
+ * 数据： 数据
+ */
